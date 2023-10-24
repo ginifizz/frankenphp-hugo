3fdad5fd021fd3ff531123e9c2a9140c1b1ff825
ginifizz <laury@les-tilleuls.coop>
2023-10-24 10:53:38 +0200

    feat: add python script

--- clone-documentation.sh
+++ clone-documentation.sh
@@ -1,31 +1,38 @@
-repo_url="git@github.com:dunglas/frankenphp.git"
+repo_url="https://$GITHUB_KEY@github.com/dunglas/frankenphp.git"
 dossier_a_cloner="docs"
 dossier_destination="$PWD/content"
 nav_destination="$PWD/content/nav.md"
 temp_dir="$PWD/temp-documentation"
 
-git clone https://$GITHUB_KEY@github.com/dunglas/frankenphp.git $temp_dir
-cp -r $temp_dir/$dossier_a_cloner $dossier_destination
+# Cloner le dépôt
+git clone "$repo_url" "$temp_dir"
 
-cp "$temp_dir/CONTRIBUTING.md" $dossier_destination
-  echo -e "---\nlayout: docs\n---\n$(cat $dossier_destination/CONTRIBUTING.md)" > "$dossier_destination/CONTRIBUTING.md"
-mv $dossier_destination/CONTRIBUTING.md $dossier_destination/contributing.md
-sed -i  '' 's|\](\([^)]*\).md)|\](/\1/)|g' $dossier_destination/contributing.md
+# Copier les fichiers nécessaires
+cp -r "$temp_dir/$dossier_a_cloner" "$dossier_destination"
+cp "$temp_dir/CONTRIBUTING.md" "$dossier_destination"
 
-cp "$temp_dir/README.md" $dossier_destination
-sed -i '' 's|src="frankenphp.png"|src="https://raw.githubusercontent.com/dunglas/frankenphp/main/frankenphp.png"|g' $dossier_destination/README.md
-mv $dossier_destination/README.md $dossier_destination/docs/_index.md
+# Modifier CONTRIBUTING.md
+mv "$dossier_destination/CONTRIBUTING.md" "$dossier_destination/contributing.md"
+sed -i '' 's|\](\([^)]*\).md)|\](/\1/)|g' "$dossier_destination/contributing.md"
 
-for file in $dossier_destination/docs/*.md; do
-  echo -e "---\nlayout: docs\n---\n$(cat $file)" > $file
-sed -i  '' 's|\](\([^)]*\).md)|\](/docs/\1)|g' $file
-sed -i '' 's|/docs/CONTRIBUTING|/contributing|g' $file
-sed -i '' 's|/docs/docs/|/docs/|g' $file
+# Modifier README.md
+cp "$temp_dir/README.md" "$dossier_destination"
+sed -i '' 's|src="frankenphp.png"|src="https://raw.githubusercontent.com/dunglas/frankenphp/main/frankenphp.png"|g' "$dossier_destination/README.md"
+mv "$dossier_destination/README.md" "$dossier_destination/docs/_index.md"
+
+# Modifier les fichiers .md dans le dossier docs
+for file in "$dossier_destination/docs"/*.md; do
+  sed -i '' -e '1s|^|---\nlayout: docs\n---\n|' "$file"
+  sed -i '' 's|\](\([^)]*\).md)|\](/docs/\1)|g' "$file"
+  sed -i '' 's|/docs/CONTRIBUTING|/contributing|g' "$file"
+  sed -i '' 's|/docs/docs/|/docs/|g' "$file"
 done
 
-sed -n '/^## Docs$/,/^##/p' "$temp_dir/README.md" | sed '/^##/d' > $nav_destination
-sed -i  '' 's|\](\([^)]*\).md)|\](/\1/)|g' $nav_destination
-sed -i '' 's|CONTRIBUTING|contributing|g' $nav_destination
-sed -i '' 's|/docs/docs/|/docs/|g' $nav_destination
-rm -rf $temp_dir
+# Extraire le contenu entre les sections "## Docs" et "##" de README.md
+sed -n '/^## Docs$/,/^##/p' "$temp_dir/README.md" | sed '/^##/d' > "$nav_destination"
+sed -i '' 's|\](\([^)]*\).md)|\](/\1/)|g' "$nav_destination"
+sed -i '' 's|CONTRIBUTING|contributing|g' "$nav_destination"
+sed -i '' 's|/docs/docs/|/docs/|g' "$nav_destination"
 
+# Supprimer le répertoire temporaire
+rm -rf "$temp_dir"
--- content/nav.md
+++ content/nav.md
@@ -1,13 +1,14 @@
+ Docs
 
-* [The worker mode](/docs/worker/)
-* [Early Hints support (103 HTTP status code)](/docs/early-hints/)
-* [Real-time](/docs/mercure/)
-* [Configuration](/docs/config/)
-* [Docker images](/docs/docker/)
-* [Compile from sources](/docs/compile/)
-* [Create static binaries](/docs/static/)
-* [Known issues](/docs/known-issues/)
+* [The worker mode](/docs/worker)
+* [Early Hints support (103 HTTP status code)](/docs/early-hints)
+* [Real-time](/docs/mercure)
+* [Configuration](/docs/config)
+* [Docker images](/docs/docker)
+* [Compile from sources](/docs/compile)
+* [Create static binaries](/docs/static)
+* [Known issues](/docs/known-issues)
 * [Demo app (Symfony) and benchmarks](https://github.com/dunglas/frankenphp-demo)
 * [Go library documentation](https://pkg.go.dev/github.com/dunglas/frankenphp)
-* [Contributing and debugging](/contributing/)
+* [Contributing and debugging](/contributing)
 
